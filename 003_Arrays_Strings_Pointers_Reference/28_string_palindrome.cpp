b75cebc61621000e65973598cbe611abefce61bd
Chaitanya <chaitanyaasole@gmail.com>
2023-09-20 07:55:07 +0530

    Did checking of palindrome using left and right method

--- 003_Arrays_Strings_Pointers_Reference/28_string_palindrome.cpp
+++ 003_Arrays_Strings_Pointers_Reference/28_string_palindrome.cpp
@@ -3,23 +3,56 @@
 using namespace std;
 int main()
 {
-    string str = "nitin";
-    string rev = str;
-    int n = str.length();
-    for (int i = 0; i < n/2; i++)
+    
     {
-        char temp = rev[i];
-        rev[i] = rev[(n-1) - i];
-        rev[(n-1)-i] = temp;
+    // string str = "nitin";
+    // string rev = str;
+    // int n = str.length();
+    // for (int i = 0; i < n/2; i++)
+    // {
+    //     char temp = rev[i];
+    //     rev[i] = rev[(n-1) - i];
+    //     rev[(n-1)-i] = temp;
+    // }
+
+    // if (str == rev)
+    // {
+    //     cout<<"palindrome\n";
+    // }
+    // else
+    // {
+    //     cout<<"not palindrome\n";
+    // }
     }
 
-    if (str == rev)
     {
-        cout<<"palindrome\n";
-    }
-    else
-    {
-        cout<<"not palindrome\n";
+        string s1 = "gfeffg";
+        int n = s1.length();
+        int left = 0;   // ! Left is index of start of string
+        int right = n - 1; // ! Right is index of end of string
+        int flag = 1;
+        while (left < right)
+        {
+            if (s1[left] == s1[right])
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                flag = 2;
+                cout<<"Not palindrome\n";
+                break;
+            } 
+            
+        }
+        if (flag == 1)
+        {
+            cout<<"Palindrome\n";   
+        }
+        
+        
+        
     }
     
     
